cb0fec584d885f6f7638b43e0f09e2cf7dfb99da
lionjove <33174402+lionjove@users.noreply.github.com>
2020-11-12 22:05:06 +0700

    Fix rubocop warning

--- src/Logic/action.rb
+++ src/Logic/action.rb
@@ -13,11 +13,22 @@ class Action
   end
 
   def do_action(valera)
-    valera.health = eval_stat_expr(valera.health, @health_expr, valera).clamp(0, 100)
-    valera.mana = eval_stat_expr(valera.mana, @mana_expr, valera).clamp(0, 100)
-    valera.cheerfulness = eval_stat_expr(valera.cheerfulness, @cheerfulness_expr, valera).clamp(-10, 10)
-    valera.fatigue = eval_stat_expr(valera.fatigue, @fatigue_expr, valera).clamp(0, 100)
+    valera.health = eval_stat_expr(valera.health, @health_expr, valera)
+    valera.mana = eval_stat_expr(valera.mana, @mana_expr, valera)
+    valera.cheerfulness = eval_stat_expr(valera.cheerfulness, @cheerfulness_expr, valera)
+    valera.fatigue = eval_stat_expr(valera.fatigue, @fatigue_expr, valera)
     valera.money = eval_stat_expr(valera.money, @money_expr, valera)
+
+    clamp_values(valera)
+  end
+
+  private
+
+  def clamp_values(valera)
+    valera.health.clamp(0, 100)
+    valera.mana.clamp(0, 100)
+    valera.cheerfulness.clamp(-10, 10)
+    valera.fatigue.clamp(0, 100)
   end
 
   def eval_stat_expr(stat, expr, _valera)
