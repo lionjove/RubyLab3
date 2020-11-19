e165826376877c34bf26f9ed77b01899858c80de
lionjove <33174402+lionjove@users.noreply.github.com>
2020-11-19 19:24:49 +0700

    implemented main menu

--- src/States/main_menu_state.rb
+++ src/States/main_menu_state.rb
@@ -4,24 +4,52 @@ require_relative 'state_interface'
 
 # Class for handling main menu logic
 class MainMenuState < StateInterface
+  def initialize
+    super
+    @next_state = 'MainMenuState'
+  end
+
   def state_name
     'MainMenuState'
   end
 
   def next_state_name
-    # leave for now
-    'GameExitState'
+    @next_state
   end
 
   def update
-    # empty for now
+    print_menu
+    input = gets.chop
+
+    handle_input(input)
   end
 
   def on_state_enter
-    puts 'hello world!'
+    # empty
   end
 
   def on_state_leave
-    # empty for now
+    # empty
+  end
+
+  private
+
+  def print_menu
+    puts '1. Start game'
+    puts '2. Load game'
+    puts '3. Exit'
+  end
+
+  def handle_input(inp)
+    case inp
+    when '1'
+      @next_state = 'GameState'
+    when '2'
+      @next_state = 'SaveMenuState'
+    when '3'
+      @next_state = 'GameExitState'
+    else
+      puts 'Bad input!'
+    end
   end
 end
