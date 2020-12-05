19f1d21521c24870d014e860d9b7ee56ec9a932b
lionjove <33174402+lionjove@users.noreply.github.com>
2020-12-05 17:57:03 +0700

    print actions on screen

--- src/Logic/game.rb
+++ src/Logic/game.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
+require_relative './valera'
+require_relative './action_loader'
+
 # Main class for game
 class Game
+  def initialize
+    @valera = Valera.new
+    action_loader = ActionLoader.new("#{__dir__}/../../data/config.json")
+    @actions = action_loader.actions
+  end
+
   def print_actions
-    puts 'Here we will print our actions'
+    counter = 1
+    @actions.each do |action|
+      puts("#{counter}. #{action.name}")
+      counter += 1
+    end
   end
 
   def handle_input(inp)
